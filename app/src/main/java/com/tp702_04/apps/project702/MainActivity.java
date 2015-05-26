6848fba6f35f9591fe4c4d9c61779cf91421180b
James Butler <jmsbtlr111@gmail.com>
2015-05-26 17:31:08 +1200

    Added Clear All Menu

--- app/src/main/java/com/tp702_04/apps/project702/MainActivity.java
+++ app/src/main/java/com/tp702_04/apps/project702/MainActivity.java
@@ -83,9 +83,11 @@ public class MainActivity extends Activity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch (id) {
+            case R.id.clear_all:
+                databaseHandler.deleteAllLogItems();
+                expandableLogListAdapter.clear();
+                expandableLogListAdapter.notifyDataSetChanged();
         }
 
         return super.onOptionsItemSelected(item);
